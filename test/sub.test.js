8f07865ef611869d32d81a70202bd408b7d62933
Sagar842483 <sagarj.chavan@lntinfotech.com>
2024-03-13 12:00:17 +0530

    code cover

---
+++ test/sub.test.js
@@ -0,0 +1,5 @@
+const sub  = require('../test/sub')
+
+test("Subtract Numebr" , () => {
+    expect(sub(4,1)).toBe(3)
+})
---
+++ test/sum.test.js
@@ -0,0 +1,6 @@
+const sum = require('../test/sum')
+
+test("Add Two Numbers " ,()=>{
+    expect(sum(1,2)).toBe(3)
+
+})
